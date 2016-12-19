f3e22ab0398fff0f164d2ad535a0b7bc66eca960
Student <Student@C0042>
2016-12-19 15:48:22 +0100

    Games started

---
+++ AnacondaMVC/AnacondaMVC/Controllers/GamesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AnacondaMVC.Games;
+
+namespace AnacondaMVC.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Wheel of Fortune
+        public ActionResult WheelOfFortune()
+        {
+            return View();
+        }
+
+        // POST: Wheel of Fortune
+        [HttpPost]
+        public ActionResult WheelOfFortune(GameResult result)
+        {
+            return View();
+        }
+    }
+}
