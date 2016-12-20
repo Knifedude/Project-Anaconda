4933672e69b02ead770e5aad57facc0cc397d994
Tim <tim.kruijsen@gmail.com>
2016-12-20 16:36:52 +0100

    Worked on slot machine

--- AnacondaMVC/AnacondaGames/Games/SlotMachine/RandomColumns.cs
+++ AnacondaMVC/AnacondaGames/Games/SlotMachine/RandomColumns.cs
@@ -9,19 +9,28 @@ namespace AnacondaGames.Games.SlotMachine
 {
     public class RandomColumns
     {
-        public void GetRandomColumnFruit()
+        public List<SlotItem> GetRandomColumnFruit()
         {
             var items = new List<RandomItem<SlotItem>>
             {
-                new RandomItem<SlotItem>(0.6, new SlotItem("Banana", 0.2)),
-                new RandomItem<SlotItem>(0.2, new SlotItem("Apple", 0.2)),
-                new RandomItem<SlotItem>(0.1, new SlotItem("Gold", 0.2)),
-                new RandomItem<SlotItem>(0.1 / 3, new SlotItem("Kiwi", 0.2)),
-                new RandomItem<SlotItem>(0.1 / 3, new SlotItem("Raspberry", 0.2)),
-                new RandomItem<SlotItem>(0.1 / 3, new SlotItem("Strawberry", 0.2))
+                new RandomItem<SlotItem>(0.15, new SlotItem("Banana", 0.5)),
+                new RandomItem<SlotItem>(0.15, new SlotItem("Apple", 0.4)),
+                new RandomItem<SlotItem>(0.1, new SlotItem("Gold", 1)),
+                new RandomItem<SlotItem>(0.2, new SlotItem("Kiwi", 0.25)),
+                new RandomItem<SlotItem>(0.2, new SlotItem("Raspberry", 0.2)),
+                new RandomItem<SlotItem>(0.2, new SlotItem("Strawberry", 0.15))
             };
             Random rand = new Random();
             var randomPicker = new RandomPicker<SlotItem>(items, rand.Next());
+
+            List<SlotItem> slotItems = new List<SlotItem>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                slotItems.Add(randomPicker.Pick().Item);
+            }
+
+            return slotItems;
         }
     }
 }
