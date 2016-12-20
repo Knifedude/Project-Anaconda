22df9f4749250f64351c21d8efd36e67ac4041fd
Christian <christianmesman@gmail.com>
2016-12-20 16:33:58 +0100

    Wallet

--- AnacondaMVC/AnacondaMVC/Controllers/WalletController.cs
+++ AnacondaMVC/AnacondaMVC/Controllers/WalletController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.WebPages;
 using AnacondaMVC.Models;
 using Microsoft.AspNet.Identity;
 
@@ -20,12 +21,12 @@ namespace AnacondaMVC.Controllers
                 var user = HttpContext.User.Identity as ClaimsIdentity;
                 var userId = user.GetUserId();
 
-                Wallet wallet = am.Wallets.FirstOrDefault(x => x.UserId.Equals(userId));
+                Wallet wallet = am.Wallets.First(x => x.UserId.Equals(userId));
 
                 if (wallet == null)
                 {
 
-                    var aspUser = am.AspNetUsers.FirstOrDefault(u => u.Id.Equals(userId));
+                    var aspUser = am.AspNetUsers.First(u => u.Id.Equals(userId));
 
 
                     wallet = new Wallet()
@@ -45,6 +46,24 @@ namespace AnacondaMVC.Controllers
 
 
             
+        }
+
+        [HttpPost]
+        public ActionResult AddCredits(FormCollection form)
+        {
+            var creditsToAdd = form["creditsToAdd"].AsInt();
+            var user = HttpContext.User.Identity as ClaimsIdentity;
+            var userId = user.GetUserId();
+
+            Wallet wal = null;
+            using (var am = new AnacondaModel())
+            {
+                wal = am.Wallets.First(w => w.UserId == userId);
+                wal.Credits += creditsToAdd;
+                am.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
